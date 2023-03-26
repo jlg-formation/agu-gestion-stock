a9221936bf33331d74615a55d44a77c9011a2ba8
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-26 13:32:16 +0200

    dev: creation of article interface
    command:
    ng g i interfaces/article
    
    using the interface to make type stronger.

--- front/src/app/services/article.service.ts
+++ front/src/app/services/article.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Article } from '../interfaces/article';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ArticleService {
-  articles = [
+  articles: Article[] = [
     { id: 'a1', name: 'Tournevis', price: 3.99, qty: 124 },
     { id: 'a2', name: 'Pelle', price: 5, qty: 45 },
   ];

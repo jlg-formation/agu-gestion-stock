0a83b4c44a8318621cf8f84ccc84f8373407b035
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-26 20:09:07 +0200

    dev: added POST method on back-end

--- back/src/api.ts
+++ back/src/api.ts
@@ -1,5 +1,9 @@
-import express from "express";
-import { Article } from "./interfaces/article";
+import express, { json } from "express";
+import { Article, NewArticle } from "./interfaces/article";
+
+const generateId = () => {
+  return Date.now() + "_" + Math.floor(Math.random() * 1e12);
+};
 
 const articles: Article[] = [
   { id: "a1", name: "Tournevis", price: 3.99, qty: 124 },
@@ -14,6 +18,15 @@ app.get("/date", (req, res) => {
 
 app.get("/articles", (req, res) => {
   res.json(articles);
+});
+
+app.use(json());
+
+app.post("/articles", (req, res) => {
+  const newArticle = req.body as NewArticle;
+  const article = { ...newArticle, id: generateId() };
+  articles.push(article);
+  res.status(201).end();
 });
 
 export default app;

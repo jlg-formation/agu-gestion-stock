bd11163823ad89a11dd900bf03ecbb88153ac8e9
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-26 13:34:42 +0200

    dev: transform article array into an observable

--- front/src/app/services/article.service.ts
+++ front/src/app/services/article.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { Article } from '../interfaces/article';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ArticleService {
-  articles: Article[] = [
+  articles$ = new BehaviorSubject<Article[]>([
     { id: 'a1', name: 'Tournevis', price: 3.99, qty: 124 },
     { id: 'a2', name: 'Pelle', price: 5, qty: 45 },
-  ];
+  ]);
   constructor() {}
 }

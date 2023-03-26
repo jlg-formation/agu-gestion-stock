e31f7c72c5a8b787ae674028a26a8136793880bd
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-26 12:44:09 +0200

    dev: added dom of stock component
    - with FontAwesome icons.

--- front/src/app/stock/stock.module.ts
+++ front/src/app/stock/stock.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { StockRoutingModule } from './stock-routing.module';
 import { StockComponent } from './stock.component';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [
-    StockComponent
-  ],
-  imports: [
-    CommonModule,
-    StockRoutingModule
-  ]
+  declarations: [StockComponent],
+  imports: [CommonModule, StockRoutingModule, FontAwesomeModule],
 })
-export class StockModule { }
+export class StockModule {}

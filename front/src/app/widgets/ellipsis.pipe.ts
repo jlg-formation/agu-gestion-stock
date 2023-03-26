82f4bf892b0210ef5f40b78da3868a7ffa68f2e0
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-26 20:29:24 +0200

    dev: implemented ellipsis pipe

--- front/src/app/widgets/ellipsis.pipe.ts
+++ front/src/app/widgets/ellipsis.pipe.ts
@@ -1,12 +1,18 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'ellipsis'
+  name: 'ellipsis',
 })
 export class EllipsisPipe implements PipeTransform {
-
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: unknown, maxLength = 10): string {
+    if (typeof value !== 'string') {
+      throw new Error(
+        'Ellipsis pipe cannot be applied on a input which is not a string.'
+      );
+    }
+    if (value.length > maxLength) {
+      return value.substring(0, maxLength) + '...';
+    }
+    return value;
   }
-
 }

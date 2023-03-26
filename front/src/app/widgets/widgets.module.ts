96a4d5b1c382e8bcfc72d28aa3dea6fdaef0bd4b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-26 17:43:19 +0200

    dev: created async-icon-button

--- front/src/app/widgets/widgets.module.ts
+++ front/src/app/widgets/widgets.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AsyncSubmitButtonComponent } from './async-submit-button/async-submit-button.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AsyncIconButtonComponent } from './async-icon-button/async-icon-button.component';
 
 @NgModule({
-  declarations: [AsyncSubmitButtonComponent],
+  declarations: [AsyncSubmitButtonComponent, AsyncIconButtonComponent],
   imports: [CommonModule, FontAwesomeModule],
-  exports: [AsyncSubmitButtonComponent],
+  exports: [AsyncSubmitButtonComponent, AsyncIconButtonComponent],
 })
 export class WidgetsModule {}

e30c428da7137600297b48f0c6f69d9cf138fde2
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-26 20:15:27 +0200

    dev: added DELETE implementation on back-end

--- back/src/api.ts
+++ back/src/api.ts
@@ -5,7 +5,7 @@ const generateId = () => {
   return Date.now() + "_" + Math.floor(Math.random() * 1e12);
 };
 
-const articles: Article[] = [
+let articles: Article[] = [
   { id: "a1", name: "Tournevis", price: 3.99, qty: 124 },
   { id: "a2", name: "Pelle", price: 5, qty: 45 },
 ];
@@ -27,6 +27,12 @@ app.post("/articles", (req, res) => {
   const article = { ...newArticle, id: generateId() };
   articles.push(article);
   res.status(201).end();
+});
+
+app.delete("/articles", (req, res) => {
+  const ids = req.body as string[];
+  articles = articles.filter((a) => !ids.includes(a.id));
+  res.status(204).end();
 });
 
 export default app;

97b5000855a254c4581564e82e86805e7e9b72c7
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-26 20:04:07 +0200

    dev: added add in http-articles

--- front/src/app/services/http-articles.service.ts
+++ front/src/app/services/http-articles.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ArticleService } from './article.service';
 import { catchError, delay, map, Observable, of, switchMap, tap } from 'rxjs';
-import { Article } from '../interfaces/article';
+import { Article, NewArticle } from '../interfaces/article';
 
 const url = 'http://localhost:3000/api/articles';
 
@@ -24,6 +24,17 @@ export class HttpArticlesService extends ArticleService {
       catchError((err) => {
         console.log('err: ', err);
         this.articles$.next([]);
+        throw err;
+      })
+    );
+  }
+
+  override add(newArticle: NewArticle): Observable<void> {
+    return of(undefined).pipe(
+      delay(500),
+      switchMap(() => this.http.post<void>(url, newArticle)),
+      catchError((err) => {
+        console.log('err: ', err);
         throw err;
       })
     );

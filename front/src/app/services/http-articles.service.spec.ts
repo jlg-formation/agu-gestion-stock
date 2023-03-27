bfb22786606f8bc7a1833e8414164517d3b10069
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-27 11:56:33 +0200

    test: coverage test on http article service

--- front/src/app/services/http-articles.service.spec.ts
+++ front/src/app/services/http-articles.service.spec.ts
@@ -5,6 +5,7 @@ import {
 } from '@angular/common/http/testing';
 
 import { HttpArticlesService, url } from './http-articles.service';
+import { newArticle } from '../test/articles.data';
 
 describe('HttpArticlesService', () => {
   let service: HttpArticlesService;
@@ -48,6 +49,52 @@ describe('HttpArticlesService', () => {
     expect(req.request.method).toEqual('GET');
     req.flush('', { status: 404, statusText: 'Not Found xxx' });
     expect(service.articles$.value).toEqual([]);
+    expect(shouldGoHere).toBe(true);
+    flush();
+  }));
+
+  it('should add', fakeAsync(() => {
+    service.add(newArticle).subscribe();
+    tick(500);
+    const req = ctrl.expectOne(url);
+    expect(req.request.method).toEqual('POST');
+    req.flush('', { status: 201, statusText: 'Created' });
+  }));
+
+  it('should add in error', fakeAsync(() => {
+    let shouldGoHere = false;
+    service.add(newArticle).subscribe({
+      error: () => {
+        shouldGoHere = true;
+      },
+    });
+    tick(500);
+    const req = ctrl.expectOne(url);
+    expect(req.request.method).toEqual('POST');
+    req.flush('', { status: 500, statusText: 'Internal Error' });
+    expect(shouldGoHere).toBe(true);
+    flush();
+  }));
+
+  it('should remove', fakeAsync(() => {
+    service.remove([]).subscribe();
+    tick(500);
+    const req = ctrl.expectOne(url);
+    expect(req.request.method).toEqual('DELETE');
+    req.flush('', { status: 204, statusText: 'No Content' });
+  }));
+
+  it('should remove in error', fakeAsync(() => {
+    let shouldGoHere = false;
+    service.remove([]).subscribe({
+      error: () => {
+        shouldGoHere = true;
+      },
+    });
+    tick(500);
+    const req = ctrl.expectOne(url);
+    expect(req.request.method).toEqual('DELETE');
+    req.flush('', { status: 500, statusText: 'Internal Error' });
     expect(shouldGoHere).toBe(true);
     flush();
   }));
---
+++ front/src/app/test/articles.data.ts
@@ -0,0 +1,7 @@
+import { NewArticle } from '../interfaces/article';
+
+export const newArticle: NewArticle = {
+  name: 'Tournevis',
+  price: 2,
+  qty: 3,
+};

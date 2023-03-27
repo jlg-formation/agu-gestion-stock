07f1bd66774ece8b0658b71b936f55be338cb9fe
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-27 12:06:46 +0200

    test: coverage article service

--- front/src/app/services/article.service.spec.ts
+++ front/src/app/services/article.service.spec.ts
@@ -1,4 +1,5 @@
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { articles, newArticle } from '../test/articles.data';
 
 import { ArticleService } from './article.service';
 
@@ -13,4 +14,48 @@ describe('ArticleService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should refresh', fakeAsync(() => {
+    service.refresh().subscribe();
+    tick(500);
+    expect(service.articles$.value).toEqual([]);
+  }));
+
+  it('should add', fakeAsync(() => {
+    service.articles = [];
+    service.add(newArticle).subscribe();
+    tick(500);
+    expect(service.articles.length).toBe(1);
+  }));
+
+  it('should add in error', fakeAsync(() => {
+    let shouldGoHere = false;
+    service.add({ ...newArticle, name: 'Trucx' }).subscribe({
+      error: () => {
+        shouldGoHere = true;
+      },
+    });
+    tick(500);
+    expect(shouldGoHere).toBe(true);
+  }));
+
+  it('should remove', fakeAsync(() => {
+    service.articles = articles;
+    service.remove(['a1']).subscribe();
+    tick(500);
+    expect(service.articles.length).toBe(1);
+  }));
+
+  it('should remove in error', fakeAsync(() => {
+    let shouldGoHere = false;
+    service.articles = articles;
+    service.remove(['a1', 'a2']).subscribe({
+      error: () => {
+        shouldGoHere = true;
+      },
+    });
+    tick(500);
+    expect(service.articles.length).toBe(2);
+    expect(shouldGoHere).toBe(true);
+  }));
 });
--- front/src/app/test/articles.data.ts
+++ front/src/app/test/articles.data.ts
@@ -1,7 +1,22 @@
-import { NewArticle } from '../interfaces/article';
+import { Article, NewArticle } from '../interfaces/article';
 
 export const newArticle: NewArticle = {
   name: 'Tournevis',
   price: 2,
   qty: 3,
 };
+
+export const articles: Article[] = [
+  {
+    id: 'a1',
+    name: 'Pelle',
+    price: 2,
+    qty: 3,
+  },
+  {
+    id: 'a2',
+    name: 'Marteau',
+    price: 2,
+    qty: 3,
+  },
+];

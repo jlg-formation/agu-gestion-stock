95057ff6d855e342d309425ea97dc74d21652262
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-27 14:03:09 +0200

    test: coverate on add component

--- front/src/app/stock/add/add.component.spec.ts
+++ front/src/app/stock/add/add.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
+import { of, throwError } from 'rxjs';
+import { ArticleService } from 'src/app/services/article.service';
 import { WidgetsModule } from 'src/app/widgets/widgets.module';
 
 import { AddComponent } from './add.component';
@@ -8,10 +10,14 @@ import { AddComponent } from './add.component';
 describe('AddComponent', () => {
   let component: AddComponent;
   let fixture: ComponentFixture<AddComponent>;
+  const articleService = new ArticleService();
 
   beforeEach(async () => {
+    articleService.add = jasmine.createSpy().and.returnValue(of(undefined));
+    articleService.refresh = jasmine.createSpy().and.returnValue(of(undefined));
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule, WidgetsModule, ReactiveFormsModule],
+      providers: [{ provide: ArticleService, useValue: articleService }],
       declarations: [AddComponent],
     }).compileComponents();
 
@@ -21,6 +27,19 @@ describe('AddComponent', () => {
   });
 
   it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should submit', () => {
+    component.submit();
+    expect(component).toBeTruthy();
+  });
+
+  it('should submit in error', () => {
+    articleService.refresh = jasmine
+      .createSpy()
+      .and.returnValue(throwError(() => new Error('oups')));
+    component.submit();
     expect(component).toBeTruthy();
   });
 });

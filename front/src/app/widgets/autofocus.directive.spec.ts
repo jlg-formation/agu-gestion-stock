1a72fcacd2416cd6e353b684593746f4b9e0ff30
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-27 14:11:48 +0200

    test: fonctional test of autofocus directive

--- front/src/app/widgets/autofocus.directive.spec.ts
+++ front/src/app/widgets/autofocus.directive.spec.ts
@@ -1,5 +1,25 @@
+import { Component } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { AutofocusDirective } from './autofocus.directive';
+
+@Component({
+  selector: 'app-test',
+  template: `<input type="text" appAutofocus />`,
+})
+export class TestComponent {}
+
 describe('AutofocusDirective', () => {
-  it('should create an instance', () => {
-    expect(1).toBeTruthy();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AutofocusDirective, TestComponent],
+    }).compileComponents();
+  });
+
+  it('should test autofocus', () => {
+    const fixture = TestBed.createComponent(TestComponent);
+    fixture.detectChanges();
+    const input = fixture.nativeElement.querySelector('input');
+    const focusElt = document.activeElement;
+    expect(focusElt).toBe(input);
   });
 });
